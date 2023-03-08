f7774be9f91295336cc69db0dbf0ea39c17e2c97
muhammad.kamran <muahmmad.kamran@appinsnap.com>
2023-03-08 11:33:10 +0500

    merged

--- app/src/main/java/com/xami/permission/myapplication/MainActivity.kt
+++ app/src/main/java/com/xami/permission/myapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.xami.permission.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.xami.permission.kami.PermissionsUtil
 
 class MainActivity : AppCompatActivity() {
@@ -11,23 +13,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         permissionsUtil = PermissionsUtil.getInstance(this@MainActivity, this.activityResultRegistry)
         lifecycle.addObserver(permissionsUtil!!)
-        requestSMSPermission()
+        val btnPermission:Button=findViewById(R.id.btnPermission)
+        btnPermission.setOnClickListener {
+            requestSMSPermission()
+        }
+
     }
 
     private fun requestSMSPermission() {
+
        permissionsUtil?.let { permission ->
+           Toast.makeText(this, "Permission", Toast.LENGTH_SHORT).show()
                 permission.requestPermission(
-                   PermissionMsg = "App required Required Permission to continue",
+                   PermissionMsg = "App Required Permission to continue",
                   permission =   arrayOf(
                         android.Manifest.permission.READ_SMS,
                         android.Manifest.permission.RECEIVE_SMS
                     ),
-                    showDialog = false,
+                    showDialog = true,
                     callBack = object : PermissionsUtil.PermissionsListenerCallback {
                         override fun onPermissionGranted() {
+                            Toast.makeText(this@MainActivity, "Permission Granted", Toast.LENGTH_SHORT).show()
                             /* Perform action on Permission Granted*/
                         }
                         override fun onPermissionCancel() {
+                            Toast.makeText(this@MainActivity, "Permission Cancel", Toast.LENGTH_SHORT).show()
                           /* Perform action on Permission Cancel*/
                         }
                     })
